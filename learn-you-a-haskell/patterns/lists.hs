0dd4c8b48f1c7690ee5790dcbc9cc03d451cbb17
Kevin O'Neil <kevinmoneil@me.com>
2017-11-21 14:37:48 +0900

    moves the files further down

---
+++ learn-you-a-haskell/patterns/lists.hs
@@ -0,0 +1,34 @@
+
+-- Uses pattern matching to extract from tuples
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+fourth :: (a, b, c, d) -> d
+fourth (_, _, _, d) = d
+
+-- Now with lists
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String  
+tell [] = "The list is empty"  
+tell (x:[]) = "The list has one element: " ++ show x  
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y  
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y 
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
